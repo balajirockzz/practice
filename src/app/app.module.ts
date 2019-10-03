6bc71fdaeb93a6e3a9ca2d8f1e3cd6b1d55d6cb8
balaji <balajiashwindx@gmail.com>
2019-10-03 12:24:07 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+import { Comp4Component } from './comp4/comp4.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component,
+    Comp4Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/comp4/comp4.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp4',
+  template: `
+    <p>
+      comp4 works!
+    </p>
+    <p> comp 44
+    </p>
+  `,
+  styles: []
+})
+export class Comp4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
